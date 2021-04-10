7fb0cbe8dc2c7f7720ccf86d21615d8f1b4154b2
Massil Taguemout <massiltag@icloud.com>
2021-04-10 19:30:13 +0200

    tweak for deployment

--- src/app/enums/api-links.enum.ts
+++ src/app/enums/api-links.enum.ts
@@ -1,14 +1,14 @@
 export enum ApiLinksEnum {
-  LIVE_DATA = 'http://infocovid-back.herokuapp.com/api/getLiveData',
-  NEWS = 'http://infocovid-back.herokuapp.com/api/news/general',
-  NEWS_VACC = 'http://infocovid-back.herokuapp.com/api/news/vaccins',
-  VAC_CENTRES = 'http://infocovid-back.herokuapp.com/api/vaccins/centres',
-  SUBSCRIBE = 'http://infocovid-back.herokuapp.com/api/subscribe',
-  METRICS = 'http://infocovid-back.herokuapp.com/api/metrics',
-  METRICS_RANGE = 'http://infocovid-back.herokuapp.com/api/metrics/range',
-  CAS_DEPARTEMENT = 'http://infocovid-back.herokuapp.com/api/metrics/departments',
-  PREVISION_CONFINEMENT = 'http://infocovid-back.herokuapp.com/api/previsions/confinement',
-  PREVISION_IMMUNITE = 'http://infocovid-back.herokuapp.com/api/previsions/immunite'
+  LIVE_DATA = 'https://infocovid-back.herokuapp.com/api/getLiveData',
+  NEWS = 'https://infocovid-back.herokuapp.com/api/news/general',
+  NEWS_VACC = 'https://infocovid-back.herokuapp.com/api/news/vaccins',
+  VAC_CENTRES = 'https://infocovid-back.herokuapp.com/api/vaccins/centres',
+  SUBSCRIBE = 'https://infocovid-back.herokuapp.com/api/subscribe',
+  METRICS = 'https://infocovid-back.herokuapp.com/api/metrics',
+  METRICS_RANGE = 'https://infocovid-back.herokuapp.com/api/metrics/range',
+  CAS_DEPARTEMENT = 'https://infocovid-back.herokuapp.com/api/metrics/departments',
+  PREVISION_CONFINEMENT = 'https://infocovid-back.herokuapp.com/api/previsions/confinement',
+  PREVISION_IMMUNITE = 'https://infocovid-back.herokuapp.com/api/previsions/immunite'
 }
 
 // tslint:disable-next-line:no-namespace
