5145cf684712b51a535aa5e52cebdd2d83666645
Massil Taguemout <massiltag@icloud.com>
2021-04-10 17:26:51 +0200

    tweak for deployment

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(requireHTTPS);
 
-app.use(express.static('./build/APP'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/build/APP'));
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: './build/APP/'});
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/build/APP/index.html'));
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
